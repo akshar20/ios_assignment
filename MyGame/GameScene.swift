7b8d7a0035d3dd162ab5c8e2e63b4577266d137b
MacStudent <macstudent@130-iMac39.local>
2019-02-21 11:00:07 -0500

    adding monsters

--- MyGame/GameScene.swift
+++ MyGame/GameScene.swift
@@ -23,5 +23,32 @@ class GameScene: SKScene {
         player.position = CGPoint(x: self.size.width/2, y: self.size.height/2)
         addChild(player)
     }
+    
+    
+    // GENERATING RANDOM POSITIONS
+    func random() -> CGFloat {
+        return CGFloat(Float(arc4random()) / 0xFFFFFFFF)
+    }
+    func random(min: CGFloat, max: CGFloat) -> CGFloat {
+        return random() * (max - min) + min
+    }
+    
+    // ADDING MOSTERS FOR COMBAT
+    func generateMonster() {
+
+        let monster = SKSpriteNode(imageNamed: "monster")
+        
+        // Determine where to spawn the monster along the Y axis
+        let actualY = random(min: monster.size.height/2, max: size.height - monster.size.height/2)
+        
+        // Position the monster slightly off-screen along the right edge,
+        // and along a random position along the Y axis as calculated above
+        monster.position = CGPoint(x: size.width + monster.size.width/2, y: actualY)
+        
+        // Add the monster to the scene
+        addChild(monster)
+        
+        
+    }
    
 }
