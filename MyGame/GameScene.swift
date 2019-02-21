18d5ae2b00f3caf15c4f066d426176eedb126d43
MacStudent <macstudent@130-iMac39.local>
2019-02-21 11:30:22 -0500

    CLEANED UP CODE

--- MyGame/GameScene.swift
+++ MyGame/GameScene.swift
@@ -6,14 +6,15 @@
 //  Copyright © 2019 MacStudent. All rights reserved.
 //
 
+// SHAPE RECOGNITION REF.: https://www.youtube.com/watch?v=ZP9tWkTx7p4&list=PLY1P2_piiWEaN_cUBbooId-AsMdg4MGZu&index=1
+
 import SpriteKit
 import GameplayKit
 
 class GameScene: SKScene {
-   
-    // REF: https://www.raywenderlich.com/71-spritekit-tutorial-for-beginners
-    
+
     let player = SKSpriteNode(imageNamed: "player")
+
     
     override func didMove(to view: SKView) {
     
@@ -23,33 +24,7 @@ class GameScene: SKScene {
         // SETTING UP THE PLAYER
         player.position = CGPoint(x: self.size.width/2, y: self.size.height/2)
         addChild(player)
-    }
-    
-    
-    // GENERATING RANDOM POSITIONS
-    func random() -> CGFloat {
-        return CGFloat(Float(arc4random()) / 0xFFFFFFFF)
-    }
-    func random(min: CGFloat, max: CGFloat) -> CGFloat {
-        return random() * (max - min) + min
-    }
-    
-    // ADDING MOSTERS FOR COMBAT
-    func generateMonster() {
-
-        let monster = SKSpriteNode(imageNamed: "monster")
-        
-        // Determine where to spawn the monster along the Y axis
-        let actualY = random(min: monster.size.height/2, max: size.height - monster.size.height/2)
-        
-        // Position the monster slightly off-screen along the right edge,
-        // and along a random position along the Y axis as calculated above
-        monster.position = CGPoint(x: size.width + monster.size.width/2, y: actualY)
-        
-        // Add the monster to the scene
-        addChild(monster)
-        
-        
+ 
     }
    
 }
