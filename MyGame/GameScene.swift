a60bbff1890056fa636eaadfb3d9f99e3bdd605f
MacStudent <macstudent@126-iMac05.local>
2019-02-25 14:02:16 -0500

    player new logo

--- MyGame/GameScene.swift
+++ MyGame/GameScene.swift
@@ -5,7 +5,7 @@
 //  Created by MacStudent on 2019-02-21.
 //  Copyright © 2019 MacStudent. All rights reserved.
 //
-
+// PLAYER IMAGE: http://3.bp.blogspot.com/-9Y6-NJcJwts/T9tmI9ebOEI/AAAAAAAADus/-0S5j_AnRio/s320/pic69716C106C0C1A4D4270D092BCC3E348.png
 // SHAPE RECOGNITION REF.: https://www.youtube.com/watch?v=ZP9tWkTx7p4&list=PLY1P2_piiWEaN_cUBbooId-AsMdg4MGZu&index=1
 
 import SpriteKit
